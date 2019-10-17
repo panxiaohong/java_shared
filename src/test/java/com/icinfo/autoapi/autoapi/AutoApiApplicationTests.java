b3f085544c380fb7db8f43077a622c553c8eee74
meng.zhao <leihong.pan@shuyun.com>
2019-10-17 13:51:56 +0800

    初始化git项目

---
+++ src/test/java/com/icinfo/autoapi/autoapi/AutoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icinfo.autoapi.autoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
